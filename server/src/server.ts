dc7a503cc2055ea54983045849f0dff8e4fd8e36
Matt Voget <matt@stoplight.io>
2021-11-21 21:15:40 -0700

    use helm for deploys

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,8 +7,12 @@ const prisma = new PrismaClient()
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.get("/", async (req: Request, res: Response) => {
+  res.json({ health: "OK" });
+});
+
 app.get("/api", async (req: Request, res: Response) => {
-  res.json({ message: "Hello, this is server!" });
+  res.json({ message: "Hello, this is the api server!" });
 });
 
 app.get("/api/users", async (req: Request, res: Response) => {
