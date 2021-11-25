197c06e33e1cf89840bf66266c3222d857fcdba0
Matt Voget <matt@stoplight.io>
2021-11-24 21:28:08 -0700

    test deploy pipeline

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello World!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : JSON.stringify(data, null, 2)}</p>
       </header>
