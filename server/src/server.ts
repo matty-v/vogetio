9b7aee389e2fd8b0726a69399939343574baefc1
Matt Voget <matt@stoplight.io>
2021-11-28 07:59:05 -0700

    cloud run server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express, { Request, Response } from 'express';
-
+import cors from 'cors';
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(cors());
 
 app.get("/", async (req: Request, res: Response) => {
   res.json({ health: "OK" });
