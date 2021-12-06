8c48fa487c402358e76785835a5425bd7ebfb400
Matt Voget <matt@stoplight.io>
2021-12-05 22:09:38 -0700

    basic outline

--- app/src/App.js
+++ app/src/App.js
@@ -22,9 +22,45 @@ export default function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>voget.io is under construction...</h1>
-      </header>
+      <div class="container">
+        <div class="row">
+          <div id="profile-pic" class="col">
+            <img src="assets/profile-pic.jpeg" alt="Profile"/>
+          </div>
+          <div class="col">
+            <div id="hello-text">
+              Hello, World!
+            </div>
+            <div id="hello-subtext">
+              Welcome to voget.io, my personal site
+            </div>
+          </div>
+          <div class="col">
+          </div>
+        </div>
+        <div class="row content-row">
+          <div class="col">
+            <div>
+              <h1>About Me</h1>
+            </div>
+            <div id="about-me-text">
+              <ul>
+                <li>Full stack software engineer</li>
+                <li>Team Lead</li>
+                <li>Dad</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row links-row">
+          <div class="col">
+            <div>
+              <a href="https://github.com/matty-v" class="fa fa-github"></a>
+              <a href="mailto: matt.voget@gmail.com" class="fa fa-envelope"></a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
