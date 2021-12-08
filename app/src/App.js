28e2c6d4e2d046fd737a3fcd3c10682788cd08af
Matt Voget <matt@stoplight.io>
2021-12-07 21:10:29 -0700

    more landing page wip

--- app/src/App.js
+++ app/src/App.js
@@ -21,39 +21,50 @@ export default function App() {
   }, []);
 
   return (
-    <div className="App">
-      <div class="container">
-        <div id="" class="row">
-          <div id="profile-pic" class="col">
-            <img src="assets/profile-pic.jpeg" alt="Profile"/>
-          </div>
-          <div class="col">
-            <div id="hello-text">
-              Hello, World!
-            </div>
-            <div id="hello-subtext">
-              You've found Matt Voget's personal site
-            </div>
-            <div id="contact-icons">
-              <a href="https://github.com/matty-v" class="fa fa-github"></a>
-              <a href="https://www.linkedin.com/in/matthew-voget-47a225a1" class="fa fa-linkedin"></a>
-              <a href="mailto: matt.voget@gmail.com" class="fa fa-envelope"></a>
-            </div>
-          </div>
-          <div class="col">
+    <>
+      <header class="mdc-top-app-bar">
+        <div class="mdc-top-app-bar__row">
+          <section class="mdc-top-app-bar__section mdc-top-app-bar__section--align-start">
+            <span id="app-title" class="mdc-top-app-bar__title">voget.io</span>
+          </section>
+          <section id="contact-icons" class="mdc-top-app-bar__section mdc-top-app-bar__section--align-end" role="toolbar">
+            <a href="https://github.com/matty-v" class="fa fa-github"></a>
+            <a href="https://www.linkedin.com/in/matthew-voget-47a225a1" class="fa fa-linkedin"></a>
+            <a href="mailto: matt.voget@gmail.com" class="fa fa-envelope"></a>
+          </section>
+        </div>
+      </header>
+      <main class="mdc-top-app-bar--fixed-adjust">
+        <div className="App">
+          <div class="container">
+            <div id="" class="row">
+              <div id="profile-pic" class="col">
+                <img src="assets/profile-pic.jpeg" alt="Profile"/>
+              </div>
+              <div class="col">
+                <div id="about-me-content">
+                  <div><i class="fa fa-code"></i>Full Stack Engineer</div>
+                  <div><i class="fa fa-users"></i>Team Lead</div>
+                  <div><i class="fa fa-heart"></i>Dad</div>
+                </div>
 
-          </div>
-        </div>
-        <div class="row content-row">
-          <div class="col">
-            <div id="about-me-content">
-              <div><i class="fa fa-code"></i>Full Stack Engineer</div>
-              <div><i class="fa fa-users"></i>Team Lead</div>
-              <div><i class="fa fa-heart"></i>Dad</div>
+              </div>
+              <div id="hello-content" class="col">
+                <div id="hello-text">
+                  Hello, World!
+                </div>
+                <div id="hello-subtext">
+                  You've found Matt Voget's personal site
+                </div>
+              </div>
+            </div>
+            <div class="row content-row">
+              <div class="col">
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
+      </main>
+</>
   );
 }
