6f4a95a43f49780d4b2f42c78fe71e58b85f5f42
Matt Voget <matt@stoplight.io>
2021-12-12 14:12:13 -0700

    post crud operations

--- server/src/auth-filter.ts
+++ server/src/auth-filter.ts
@@ -27,6 +27,8 @@ authFilter.use(async (req: Request, res: Response, next: NextFunction) => {
     return res.status(400).json(notAuthorizedResponse);
   }
 
+  req['email'] = data.email;
+
   next();
 });
 
--- server/src/posts-controller.ts
+++ server/src/posts-controller.ts
@@ -1,19 +1,100 @@
 import { Router, Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
 import authFilter from './auth-filter';
 
-const postController = Router();
+const prisma = new PrismaClient();
 
+/**
+ * /api/posts
+ */
+const postController = Router();
 postController.use(authFilter);
 
-// Route: /api/posts
-postController.get('/', (req: Request, res: Response) => {
+// Get all posts
+postController.get('/', async (req: Request, res: Response) => {
 
-  const testResponse = [{
-    name:'post 1',
-    content: 'post 1 content'
-  }];
+  const posts = await prisma.post.findMany({
+    where: {
+      author: {
+        email: req['email']
+      }
+    }
+  });
 
-  return res.json(testResponse);
+  return res.json(posts);
 });
+
+// Create new post
+postController.post('/', async (req: Request, res: Response) => {
+
+  if (!req.body.title) return res.status(400).json({ message: 'Invalid request: Post is missing a title!' });
+  if (!req.body.content) return res.status(400).json({ message: 'Invalid request: Post is missing content!' });
+
+  const newPost = await prisma.post.create({
+    data: {
+      title: req.body.title,
+      content: req.body.content,
+      author: {
+        connect: {
+          email: req['email']
+        }
+      }
+    }
+  })
+
+  return res.json(newPost);
+});
+
+// Update a post
+postController.put('/:id', async (req: Request, res: Response) => {
+
+  const title = req.body.title || undefined;
+  const content = req.body.content || undefined;
+
+  if (!title && !content) return res.status(400).send({ message: 'Invalid request: did not provide post data to update (title/content)' });
+
+  const postDataToUpdate = {
+    ...title && ({title}),
+    ...content && ({content}),
+  }
+
+  const postToUpdate = await prisma.post.findUnique({
+    where: {
+      id: req.params.id
+    }
+  });
+
+  if (!postToUpdate) return res.status(400).send({ message: `Could not find a post with ID: ${req.params.id}` });
+
+  const updatedPost = await prisma.post.update({
+    where: {
+      id: postToUpdate.id
+    },
+    data : postDataToUpdate
+  })
+
+  return res.json(updatedPost);
+});
+
+// Delete a post
+postController.delete('/:id', async (req: Request, res: Response) => {
+
+  const postToDelete = await prisma.post.findUnique({
+    where: {
+      id: req.params.id
+    }
+  });
+
+  if (!postToDelete) return res.status(400).send({ message: `Could not find a post with ID: ${req.params.id}` });
+
+  const deletedPost = await prisma.post.delete({
+    where: {
+      id: postToDelete.id
+    }
+  })
+
+  return res.json(deletedPost);
+});
+
 
 export default postController;
