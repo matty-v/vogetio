04fa3ce2921006490b16af1a4ca728d77457e66e
Matt Voget <matt.voget@gmail.com>
2023-12-03 20:43:55 -0700

    maybe this time

--- app/server.js
+++ app/server.js
@@ -5,10 +5,8 @@ const DIST_DIR = path.join(__dirname, "dist");
 const PORT = 3000;
 const app = express();
 
-//Serving the files on the dist folder
 app.use(express.static(DIST_DIR));
 
-//Send index.html when the user access the web
 app.get("*", function (req, res) {
   res.sendFile(path.join(DIST_DIR, "index.html"));
 });
